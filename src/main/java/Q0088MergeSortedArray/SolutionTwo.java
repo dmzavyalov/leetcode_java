2d1ff11de0d424cd70b1bb39f036fdfd950b7b1d
Dmytro Zavialov <dmzavyalov@gmail.com>
2024-02-25 19:21:06 +0200

    Q0088 Merge sorted array. Improved version with constant space complexity

---
+++ src/main/java/Q0088MergeSortedArray/SolutionTwo.java
@@ -0,0 +1,30 @@
+package Q0088MergeSortedArray;
+
+/*
+
+https://leetcode.com/problems/merge-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
+
+Topics: Array, Two Pointers, Sorting
+
+Time: O(m+n)
+Space: O(1)
+
+Iterate backwards
+
+ */
+
+public class SolutionTwo {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int pos1 = m - 1;
+        int pos2 = n - 1;
+        int insertPos = m + n - 1;
+
+        while (pos2 >= 0) {
+            if (pos1 >= 0 && nums1[pos1] > nums2[pos2]) {
+                nums1[insertPos--] = nums1[pos1--];
+            } else {
+                nums1[insertPos--] = nums2[pos2--];
+            }
+        }
+    }
+}
