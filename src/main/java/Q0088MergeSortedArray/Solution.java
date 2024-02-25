8f17a5fb143afa6cd506156426955c19e219a8cc
Dmytro Zavialov <dmzavyalov@gmail.com>
2024-02-25 19:06:44 +0200

    Q0088 Merge sorted array

--- src/main/java/Q0088MergeSortedArray/Solution.java
+++ src/main/java/Q0088MergeSortedArray/Solution.java
@@ -1,11 +1,32 @@
 package Q0088MergeSortedArray;
 
 /*
+
 https://leetcode.com/problems/merge-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
+
+Topics: Array, Two Pointers, Sorting
+
+Time: O(m+n)
+Space: O(m+n)
+
  */
 
 public class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] merged = new int[m+n];
+        int insertPos = 0, pos1 = 0, pos2 = 0;
 
+        while(insertPos < m + n) {
+            if (pos1 >= m) {
+                merged[insertPos++] = nums2[pos2++];
+            } else if (pos2 >= n) {
+                merged[insertPos++] = nums1[pos1++];
+            } else {
+                merged[insertPos++] = nums2[pos2] < nums1[pos1] ?
+                    nums2[pos2++] : nums1[pos1++];
+            }
+        }
+
+        System.arraycopy(merged, 0, nums1, 0, m + n);
     }
 }
---
+++ src/test/java/Q0088MergeSortedArray/SolutionTest.java
@@ -0,0 +1,46 @@
+package Q0088MergeSortedArray;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+class SolutionTest {
+
+    @Test
+    void testOne() {
+        var nums1 = new int[] { 1, 2, 3, 0, 0, 0 };
+        final int m = 3;
+        final var nums2 = new int[] { 2, 5, 6 };
+        final int n = 3;
+
+        final var solution = new Solution();
+        solution.merge(nums1, m, nums2, n);
+        final var expected = new int[] { 1, 2, 2, 3, 5, 6};
+        assertArrayEquals(nums1, expected);
+    }
+
+    @Test
+    void testTwo() {
+        var nums1 = new int[] { 1 };
+        final int m = 1;
+        final var nums2 = new int[] { };
+        final int n = 0;
+
+        final var solution = new Solution();
+        solution.merge(nums1, m, nums2, n);
+        final var expected = new int[] { 1 };
+        assertArrayEquals(nums1, expected);
+    }
+
+    @Test
+    void testThree() {
+        var nums1 = new int[] { 0 };
+        final int m = 0;
+        final var nums2 = new int[] { 1 };
+        final int n = 1;
+
+        final var solution = new Solution();
+        solution.merge(nums1, m, nums2, n);
+        final var expected = new int[] { 1 };
+        assertArrayEquals(nums1, expected);
+    }
+
+}
