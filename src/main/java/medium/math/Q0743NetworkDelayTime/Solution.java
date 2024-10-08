9a74cd82d7a64faa10f085fd82869bf523845f11
Dmytro Zavialov <dmytro.zavialov.it@gmail.com>
2024-10-08 10:18:00 +0300

    Q0743 Network delay time

---
+++ src/main/java/medium/math/Q0743NetworkDelayTime/Solution.java
@@ -0,0 +1,52 @@
+package medium.math.Q0743NetworkDelayTime;
+
+/*
+https://leetcode.com/problems/network-delay-time/
+Dijkstra's algorithm
+Time: E log V
+Space: O(V^2)
+ */
+
+import java.util.*;
+
+public class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        Map<Integer, List<int[]>> edges = new HashMap<>();
+
+        //from, to, time
+        for (int[] e : times) {
+            edges
+                .computeIfAbsent(e[0], v -> new ArrayList<>())  //from
+                .add(new int[]{e[1], e[2]});    //to, time
+        }
+
+        //path cost, node number
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        minHeap.offer(new int[]{0, k});   //Starting node
+
+        Set<Integer> visited = new HashSet<>();
+        int t = 0;
+
+        while (!minHeap.isEmpty()) {
+            int[] entry = minHeap.poll();
+            int w1 = entry[0], n1 = entry[1];
+            if (visited.contains(n1)) {
+                continue;
+            }
+            visited.add(n1);
+            t = Math.max(t, w1);
+
+            if (edges.containsKey(n1)) { //It's possible that the node has no outgoing edges
+                //Loop through all the node neighbors
+                for (int[] e : edges.get(n1)) {
+                    int n2 = e[0], w2 = e[1];
+                    if (!visited.contains(n2)) {
+                        minHeap.offer(new int[]{w2 + w1, n2});
+                    }
+                }
+            }
+        }
+
+        return visited.size() == n ? t : -1;
+    }
+}
---
+++ src/test/java/medium/math/Q0743NetworkDelayTime/SolutionTest.java
@@ -0,0 +1,22 @@
+package medium.math.Q0743NetworkDelayTime;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+    @Test
+    void testOne() {
+        assertEquals(2, new Solution().networkDelayTime(new int[][]{{2,1,1},{2,3,1},{3,4,1}}, 4,2));
+    }
+
+    @Test
+    void testTwo() {
+        assertEquals(1, new Solution().networkDelayTime(new int[][]{{1,2,1}}, 2,1));
+    }
+
+    @Test
+    void testThree() {
+        assertEquals(-1, new Solution().networkDelayTime(new int[][]{{1,2,1}}, 2,2));
+    }
+}
